550bee26dd5d987eda5f888a452533e7b1c9c480
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-10 17:18:20 +0530

    Add responsive Navbar component with mobile dropdown menu

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>This is my Sweet home</p>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
 ]);
