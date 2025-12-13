1b08e121d8e6e52ca4e9ffd992669de5f1f97f7c
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-13 21:03:24 +0530

    Add search form with icon input field for location/district search

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -8,28 +8,52 @@ const Coverage = () => {
   const serviceCenter = useLoaderData();
   console.log(serviceCenter);
   return (
-    <div>
-      <h2 className="text-xl font-bold text-teal-900 py-3 lg:py-10 text-center  lg:text-5xl">
-        We are available in 750 districts
+    <div className="w-11/12 mx-auto">
+      <h2 className="text-xl font-bold text-teal-900 py-3 lg:py-10 text-left  lg:text-5xl">
+        We are available in 15 districts
       </h2>
-      <div className=""></div>
+      <div className=" py-3 lg:py-8">
+        {/* search */}
+        <form>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" />
+          </label>
+        </form>
+      </div>
       {/*  */}
-      <div className=" w-11/12 mx-auto h-[1000px]">
+      <div className=" h-[1000px]">
         <MapContainer
           center={position}
           zoom={8}
           scrollWheelZoom={false}
-          className="h-[500px]"
+          className="h-[700px]"
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {serviceCenter.map((center) => (
-            <Marker position={[center.latitude, center.longitude]}>
+          {serviceCenter.map((center, index) => (
+            <Marker key={index} position={[center.latitude, center.longitude]}>
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                <strong>{center.district}</strong> <br /> Service Area:{" "}
+                {center.covered_area.join(", ")} .
               </Popup>
             </Marker>
           ))}
