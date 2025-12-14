2a3a471c657e0bc9bdf9dc0ed033e19ff9706f0b
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-14 21:03:34 +0530

    Implement registration form with email and password fields using React Hook Form

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const { register } = useForm();
+
   return (
     <div>
-      <p>I am register</p>
+      <form>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email")}
+            className="input"
+            placeholder="Email"
+          />
+          {/* password */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password")}
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
     </div>
   );
 };
