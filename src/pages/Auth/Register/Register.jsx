87e7092c1acaf037f95bc864511233c26ed29947
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-16 17:21:14 +0530

    Add user registration form with validation using react-hook-form

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -25,49 +25,56 @@ const Register = () => {
   return (
     <div className="">
       <form onSubmit={handleSubmit(handleRegistration)}>
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input
-            type="email"
-            {...register("email", { required: true })}
-            className="input"
-            placeholder="Email"
-          />
-          {errors.email?.type === "required" && (
-            <p className="text-red-600 font-semibold">Email is required.</p>
-          )}
-          {/* password */}
-          <label className="label">Password</label>
-          <input
-            type="password"
-            {...register("password", {
-              required: true,
-              minLength: 6,
-              pattern:
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-            })}
-            className="input"
-            placeholder="Password"
-          />
-          {errors.password?.type === "required" && (
-            <p className="text-red-600 font-semibold">Password is required.</p>
-          )}
-          {errors.password?.type === "minLength" && (
-            <p className="text-red-600 font-semibold">
-              Password must be 6 character or longer.
-            </p>
-          )}
-          {errors.password?.type === "pattern" && (
-            <p className="text-red-600">
-              Password must have at least one uppercase , at least one lowercase
-              , at least one number, and at least one speical character
-            </p>
-          )}
-          <div>
-            <a className="link link-hover">Forgot password?</a>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0">
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                className="input"
+                placeholder="Email"
+              />
+              {errors.email?.type === "required" && (
+                <p className="text-red-600 font-semibold">Email is required.</p>
+              )}
+              {/* password */}
+              <label className="label">Password</label>
+              <input
+                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+                })}
+                className="input"
+                placeholder="Password"
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600 font-semibold">
+                  Password is required.
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600 font-semibold">
+                  Password must be 6 character or longer.
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must have at least one uppercase , at least one
+                  lowercase , at least one number, and at least one speical
+                  character
+                </p>
+              )}
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn  mt-4">Register</button>
+            </fieldset>
           </div>
-          <button className="btn btn-neutral mt-4">Register</button>
-        </fieldset>
+        </div>
       </form>
     </div>
   );
