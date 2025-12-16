e6780845caf27ca558420e215e3c439048c36050
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-16 17:22:15 +0530

    Add login form component with email/password fields and Forgot Password link

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Login = () => {
-  return <p></p>;
+  return (
+    <div className="w-11/12 mx-auto py-3 lg:py-6">
+      <h2 className="font-bold text-teal-800 text-xl md:text-3xl lg:text-6xl ">
+        Welcome Back
+      </h2>
+      <p className="text-black py-2 font-semibold">Login with ZapShift</p>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 ">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
