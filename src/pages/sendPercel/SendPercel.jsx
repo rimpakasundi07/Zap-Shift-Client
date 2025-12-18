9116a881c950fc49b362c357291696a6f2f843dd
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-18 22:04:27 +0530

    Add SendParcel form component with radio buttons for parcel type selection

--- src/pages/sendPercel/SendPercel.jsx
+++ src/pages/sendPercel/SendPercel.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const SendPercel = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSendPercel = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-11/12 mx-auto py-2 lg:py-8">
       <div className="">
@@ -12,7 +23,44 @@ const SendPercel = () => {
         </p>
       </div>
       {/* form */}
-      <div className=""></div>
+      <div className="">
+        <form onSubmit={handleSubmit(handleSendPercel)} className="mt-12 p-10">
+          {/* parcel type*/}
+          <div className="">
+            <label className="label mr-4">
+              <input
+                type="radio"
+                {...register("parcelType")}
+                value="document"
+                defaultChecked
+                className="radio bg-blue-100 border-blue-300 checked:bg-blue-200 checked:text-blue-600 checked:border-blue-600 "
+              />
+              Document
+            </label>
+            <label className="label">
+              <input
+                type="radio"
+                {...register("parcelType")}
+                value="non-document"
+                defaultChecked
+                className="radio bg-red-100 border-red-300 checked:bg-red-200 checked:text-red-600 checked:border-red-600
+                 "
+              />
+              Non-Document
+            </label>
+          </div>
+          {/* parcel info */}
+          <div className=""></div>
+          {/* two column */}
+          <div className="">
+            {/* 1st div  || sender info  */}
+            <div className=""></div>
+            {/* 2nd div  || receiver info */}
+            <div className=""></div>
+          </div>
+          <input type="submit" className="btn" value="Send Parcel" />
+        </form>
+      </div>
     </div>
   );
 };
