6769ec297c805a455f59c76f17eb1ab1ddfb75e7
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-18 22:36:58 +0530

    Add parcel name and weight input fields in grid layout for Send Parcel form

--- src/pages/sendPercel/SendPercel.jsx
+++ src/pages/sendPercel/SendPercel.jsx
@@ -21,19 +21,23 @@ const SendPercel = () => {
         <p className="font-bold text-lg py-2 lg:py-5 lg:text-xl">
           Enter your parcel details
         </p>
+        <hr className="bg-gray-500"></hr>
       </div>
       {/* form */}
       <div className="">
-        <form onSubmit={handleSubmit(handleSendPercel)} className="mt-12 p-10">
+        <form
+          onSubmit={handleSubmit(handleSendPercel)}
+          className="mt-6 p-10 text-black"
+        >
           {/* parcel type*/}
           <div className="">
-            <label className="label mr-4">
+            <label className="label mr-6 lg:mr-16">
               <input
                 type="radio"
                 {...register("parcelType")}
                 value="document"
                 defaultChecked
-                className="radio bg-blue-100 border-blue-300 checked:bg-blue-200 checked:text-blue-600 checked:border-blue-600 "
+                className="radio bg-blue-100 border-blue-300 checked:bg-blue-200 checked:text-blue-600 checked:border-blue-600"
               />
               Document
             </label>
@@ -43,14 +47,36 @@ const SendPercel = () => {
                 {...register("parcelType")}
                 value="non-document"
                 defaultChecked
-                className="radio bg-red-100 border-red-300 checked:bg-red-200 checked:text-red-600 checked:border-red-600
-                 "
+                className="radio bg-red-100 border-red-300 checked:bg-red-200 checked:text-red-600 checked:border-red-600"
               />
               Non-Document
             </label>
           </div>
           {/* parcel info */}
-          <div className=""></div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-32 py-4 lg:py-6 ">
+            <fieldset className="fieldset">
+              <label className="label text-lg font-bold text-gray-600">
+                Parcel Name
+              </label>
+              <input
+                type="text"
+                {...register("parcelName")}
+                className="input w-full"
+                placeholder="Enter Parcel Name"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <label className="label text-lg font-bold text-gray-600">
+                Parcel Weight (kg)
+              </label>
+              <input
+                type="text"
+                {...register("parcelWeight")}
+                className="input  w-full"
+                placeholder="Parcel Weight"
+              />
+            </fieldset>
+          </div>
           {/* two column */}
           <div className="">
             {/* 1st div  || sender info  */}
@@ -60,7 +86,7 @@ const SendPercel = () => {
           </div>
           <input
             type="submit"
-            className="btn bg-[#CAEB66] hover:bg-teal-800 cursor-pointer hover:text-white text-black"
+            className="btn my-3 lg:my-6 bg-[#CAEB66] hover:bg-teal-800 cursor-pointer hover:text-white text-black"
             value="Send Parcel"
           />
         </form>
