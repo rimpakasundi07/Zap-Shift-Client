ef933fd624588a29a75d0cbd7c9dd71327391485
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-21 21:50:51 +0530

    Add handleParcelDelete function with SweetAlert confirmation

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -4,6 +4,7 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useAuth();
@@ -16,6 +17,31 @@ const MyParcels = () => {
       return res.data;
     },
   });
+
+  const handleParcelDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          console.log(res.data);
+        });
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+      }
+    });
+  };
+
   return (
     <div>
       <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
@@ -47,7 +73,10 @@ const MyParcels = () => {
                   <button className="btn btn-square hover:bg-primary">
                     <FaMagnifyingGlass />
                   </button>
-                  <button className="btn btn-square hover:bg-primary">
+                  <button
+                    onClick={() => handleParcelDelete(parcel._id)}
+                    className="btn btn-square hover:bg-primary"
+                  >
                     <FaTrashAlt />
                   </button>
                 </td>
