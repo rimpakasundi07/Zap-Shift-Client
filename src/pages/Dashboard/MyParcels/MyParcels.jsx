d98882ae4ea0dc5ab14870fd92a08cdc6a9d3e2e
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-21 20:51:16 +0530

    Update MyParcels table with centered text, action buttons and icons

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const MyParcels = () => {
   const { user } = useAuth();
@@ -24,21 +26,31 @@ const MyParcels = () => {
           {/* head */}
           <thead>
             <tr>
-              <th className="text-green-700"></th>
-              <th className="text-green-700">Name</th>
-              <th className="text-green-700">Cost</th>
-              <th className="text-green-700">Payment Status</th>
-              <th className="text-green-700">Actions</th>
+              <th className="text-green-700  text-center"></th>
+              <th className="text-green-700  text-center">Name</th>
+              <th className="text-green-700  text-center">Cost</th>
+              <th className="text-green-700  text-center">Payment Status</th>
+              <th className="text-green-700 text-center">Actions</th>
             </tr>
           </thead>
           <tbody>
             {parcels.map((parcel, index) => (
               <tr key={parcel._id}>
                 <th>{index + 1}</th>
-                <td>{parcel.parcelName}</td>
-                <td>{parcel.cost}</td>
-                <td>Blue</td>
-                <td>Blue</td>
+                <td className=" text-center">{parcel.parcelName}</td>
+                <td className=" text-center">{parcel.cost}</td>
+                <td className=" text-center">Blue</td>
+                <td className="mx-2 space-x-5  text-center">
+                  <button className="btn btn-square hover:bg-primary">
+                    <FaRegEdit />
+                  </button>
+                  <button className="btn btn-square hover:bg-primary">
+                    <FaMagnifyingGlass />
+                  </button>
+                  <button className="btn btn-square hover:bg-primary">
+                    <FaTrashAlt />
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
