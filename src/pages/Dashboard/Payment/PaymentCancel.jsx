3f432aa4f76156e1982d6f6827e08c2a86e63463
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-24 20:53:05 +0530

    Add PaymentCancel component with error message and retry prompt

---
+++ src/pages/Dashboard/Payment/PaymentCancel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PaymentCancel = () => {
+  return (
+    <div>
+      <h2 className="lg:text-4xl text-center py-4 lg:py-20 font-bold text-red-600 text-xl">
+        Payment is Cancelled !!!
+      </h2>
+      <p className="text-xl text-center py-4 lg:py-20 font-bold">
+        Please try again.
+      </p>
+    </div>
+  );
+};
+
+export default PaymentCancel;
