24bbd8b03152a4cf1a2a040fd441739d812e9f5f
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-26 19:31:47 +0530

    Update PaymentSuccess to display transaction ID and tracking ID after payment

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const sessionId = searchParams.get("session_id");
   const axiosSecure = useAxiosSecure();
 
@@ -15,15 +16,25 @@ const PaymentSuccess = () => {
         .patch(`/payment-success?session_id=${sessionId}`)
         .then((res) => {
           console.log(res.data);
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
         });
     }
   }, [sessionId, axiosSecure]);
 
   return (
-    <div>
-      <h2 className="lg:text-4xl text-center py-4 lg:py-20 font-bold text-teal-800 text-xl">
+    <div className="text-center">
+      <h2 className="lg:text-4xl py-4 lg:pt-20 lg:pb-10 font-bold text-teal-800 text-xl">
         Payment Succesfull !
       </h2>
+      <p className="text-xl pt-2 font-semibold">
+        Your TransactionId: {paymentInfo.transactionId}
+      </p>
+      <p className="text-xl pt-2 font-semibold">
+        Your Parcel Tracking id: {paymentInfo.trackingId}{" "}
+      </p>
     </div>
   );
 };
