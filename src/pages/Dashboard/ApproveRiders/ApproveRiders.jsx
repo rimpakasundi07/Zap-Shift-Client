56b7e14020bdd1936129a1c605421df438251775
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-29 22:13:43 +0530

    Add riders table with dynamic mapping and zebra styling

--- src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
+++ src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
@@ -4,7 +4,7 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ApproveRiders = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: rider = [] } = useQuery({
+  const { data: riders = [] } = useQuery({
     queryKey: ["riders", "pending"],
     queryFn: async () => {
       const res = await axiosSecure.get("/riders");
@@ -14,8 +14,33 @@ const ApproveRiders = () => {
   return (
     <div>
       <h2 className="lg:text-5xl text-center lg:py-6 text-xl text-teal-800 font-bold">
-        Riders pending approval : {rider.length}
+        Riders pending approval : {riders.length}
       </h2>
+      <div className="">
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {riders.map((rider, index) => (
+                <tr>
+                  <th>{index + 1} </th>
+                  <td>Cy Ganderton</td>
+                  <td>Quality Control Specialist</td>
+                  <td>Blue</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
