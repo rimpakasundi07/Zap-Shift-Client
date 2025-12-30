49295a62b55497389b7f74d5506769fe53e1d347
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-30 21:51:29 +0530

    Add conditional admin role display with promote/demote buttons

--- src/pages/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/pages/Dashboard/UsersManagement/UsersManagement.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaUserShield } from "react-icons/fa6";
+import { FaUserTimes } from "react-icons/fa";
 
 const UsersManagement = () => {
   const axiosSecure = useAxiosSecure();
@@ -19,7 +21,7 @@ const UsersManagement = () => {
       </p>
       {/* table */}
       <div className="">
-        <div className="overflow-x-auto">
+        <div className="">
           <table className="table">
             {/* head */}
             <thead>
@@ -27,6 +29,7 @@ const UsersManagement = () => {
                 <th></th>
                 <th className=" text-xl font-bold">Name</th>
                 <th className="text-center text-xl font-bold">Email</th>
+                <th className="text-center text-xl font-bold">Role</th>
                 <th className="text-center text-xl font-bold">Admin Action</th>
                 <th className="text-center text-xl font-bold">Other Actions</th>
               </tr>
@@ -36,6 +39,7 @@ const UsersManagement = () => {
               {users.map((user, index) => (
                 <tr>
                   <td>{index + 1}</td>
+                  {/* photo & name */}
                   <td>
                     <div className="flex items-center gap-3">
                       <div className="avatar">
@@ -47,16 +51,31 @@ const UsersManagement = () => {
                         </div>
                       </div>
                       <div className="text-center">
-                        <div className="font-bold text-center">
+                        <div className="font-bold  text-xl lg:text-2xl text-center">
                           {user.displayName}
                         </div>
                       </div>
                     </div>
                   </td>
+                  {/* email */}
                   <td className="text-center text-xl lg:text-2xl text-sky-400 font-bold">
                     {user.email}
                   </td>
-                  <td className="text-center font-bold">Admin</td>
+                  {/* role */}
+                  <td className="text-center text-xl lg:text-2xl font-bold">
+                    {user.role}
+                  </td>
+                  <td className="text-center font-bold">
+                    {user.role === "admin" ? (
+                      <button className="btn">
+                        <FaUserTimes />
+                      </button>
+                    ) : (
+                      <button className="btn">
+                        <FaUserShield />
+                      </button>
+                    )}
+                  </td>
                   <td className="text-center font-bold">Actions :</td>
                 </tr>
               ))}
