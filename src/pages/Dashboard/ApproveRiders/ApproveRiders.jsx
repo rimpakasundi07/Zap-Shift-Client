b72d4b20e77c13eb7d65dcfb12fc6080a811fe6d
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-30 12:10:33 +0530

    Add handleApproval function with PATCH request and success/error alerts

--- src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
+++ src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { IoPersonRemove } from "react-icons/io5";
+import { FaUserCheck } from "react-icons/fa6";
+import { IoTrash } from "react-icons/io5";
+import Swal from "sweetalert2";
 
 const ApproveRiders = () => {
   const axiosSecure = useAxiosSecure();
@@ -11,6 +15,36 @@ const ApproveRiders = () => {
       return res.data;
     },
   });
+
+  // to addd  || approved user
+
+  const handleApproval = (id) => {
+    const updateInfo = { status: "approved" };
+
+    axiosSecure
+      .patch(`/riders/${id}`, updateInfo)
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Rider's has been approved !",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Submission Error:", error);
+
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong! Please try again.",
+        });
+      });
+  };
+
   return (
     <div>
       <h2 className="lg:text-5xl text-center lg:py-6 text-xl text-teal-800 font-bold">
@@ -23,18 +57,35 @@ const ApproveRiders = () => {
             <thead>
               <tr>
                 <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th className="text-center text-xl font-bold">Name</th>
+                <th className="text-center text-xl font-bold">Email</th>
+                <th className="text-center text-xl font-bold">Status</th>
+                <th className="text-center text-xl font-bold">District</th>
+                <th className="text-center text-xl font-bold">Actions</th>
               </tr>
             </thead>
             <tbody>
               {riders.map((rider, index) => (
                 <tr>
                   <th>{index + 1} </th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
+                  <td className="text-center">{rider.name}</td>
+                  <td className="text-center">{rider.email}</td>
+                  <td className="text-center font-bold">{rider.status}</td>
+                  <td className="text-center">{rider.district}</td>
+                  <td className="space-x-3 text-center">
+                    <button
+                      onClick={() => handleApproval(rider._id)}
+                      className="btn hover:bg-green-400 "
+                    >
+                      <FaUserCheck />
+                    </button>
+                    <button className="btn hover:bg-red-500">
+                      <IoPersonRemove />
+                    </button>
+                    <button className="btn hover:bg-gray-300">
+                      <IoTrash />
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
