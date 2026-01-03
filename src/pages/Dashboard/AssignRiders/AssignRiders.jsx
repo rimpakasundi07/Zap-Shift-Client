898e10b992fac226b754ac66b9a46ecf36648c52
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-03 18:19:54 +0530

    Add table structure for Assign Riders with sample data

--- src/pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -18,6 +18,30 @@ const AssignRiders = () => {
       <p className="lg:text-5xl text-center lg:py-6 text-xl text-teal-800 font-bold">
         Assign Riders: {parcels.length}
       </p>
+      {/* table */}
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
