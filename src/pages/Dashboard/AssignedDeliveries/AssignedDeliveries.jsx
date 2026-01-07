b6a6561721fb05301c5fadfe7396c4b4d89ad8c5
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-07 21:42:45 +0530

    Add handleAcceptDelivery function for riders to accept parcel assignments

--- src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
+++ src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AssignedDeliveries = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["parcels", user.email, "driver_assigned"],
     queryFn: async () => {
       const res = await axiosSecure.get(
@@ -15,6 +16,25 @@ const AssignedDeliveries = () => {
       return res.data;
     },
   });
+
+  const handleAcceptDelivery = (parcel) => {
+    const statusInfo = { deliveryStatus: "rider_arriving" };
+    axiosSecure
+      .patch(`/parcels/${parcel._id}/status`, statusInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Thank you for accepting.`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   return (
     <div>
       <h2 className="lg:text-5xl text-center lg:py-6 text-xl text-teal-800 font-bold">
@@ -37,7 +57,12 @@ const AssignedDeliveries = () => {
                 <th>{index + 1} </th>
                 <td>{parcel.parcelName}</td>
                 <td className="space-x-3">
-                  <button className="btn btn-primary text-black">Accept</button>
+                  <button
+                    onClick={() => handleAcceptDelivery(parcel)}
+                    className="btn btn-primary text-black"
+                  >
+                    Accept
+                  </button>
                   <button className="btn btn-outline btn-error">Reject</button>
                 </td>
                 <td>Blue</td>
