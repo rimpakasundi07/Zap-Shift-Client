c04711046af105e9284aa0512efcad3ee18071c6
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-10 21:20:11 +0530

    Add table for completed deliveries with rider assignment functionality

--- src/pages/Dashboard/CompletedDeliveries/CompletedDeliveries.jsx
+++ src/pages/Dashboard/CompletedDeliveries/CompletedDeliveries.jsx
@@ -23,6 +23,42 @@ const CompletedDeliveries = () => {
       <h2 className="lg:text-5xl text-center lg:py-6 text-xl text-teal-800 font-bold">
         Completed Deliveries : {parcels.length}
       </h2>
+
+      {/* table format */}
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th className="text-center  text-xl font-bold">Name</th>
+              <th className="text-center  text-xl font-bold">Cost</th>
+              <th className="text-center  text-xl font-bold">Created At</th>
+              <th className="text-center  text-xl font-bold">
+                Pickup District
+              </th>
+              <th className="text-center  text-xl font-bold">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td className="text-center">{parcel.parcelName}</td>
+                <td className="text-center">{parcel.cost}</td>
+                <td className="text-center">{parcel.createdAt}</td>
+                <td className="text-center">{parcel.senderDistrict}</td>
+                <td className="text-center">
+                  <button className="btn btn-primary text-black">
+                    Find Riders
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
