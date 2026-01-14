3e914de869545ce18a61be9ec29c8bab362e6857
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-14 22:17:26 +0530

    Add AdminDashboardHome component for admin dashboard landing page

---
+++ src/pages/Dashboard/DashboardHome/RiderDashboardHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RiderDashboardHome = () => {
+  return (
+    <div>
+      <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
+        Rider Dashing
+      </h2>
+    </div>
+  );
+};
+
+export default RiderDashboardHome;
