18a0fa90ae825efd4ecceb887e30e419dea96599
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-14 22:24:08 +0530

    Add UserDashboardHome component for user dashboard landing page

---
+++ src/pages/Dashboard/DashboardHome/UserDashboardHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserDashboardHome = () => {
+  return (
+    <div>
+      <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
+        User Dashing
+      </h2>
+    </div>
+  );
+};
+
+export default UserDashboardHome;
