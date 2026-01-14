e0bfb4452a76f343fb64d33cd38f19f10d9f2a58
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-14 22:09:21 +0530

    Add AdminDashboardHome component for admin dashboard landing page

---
+++ src/pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminDashboardHome = () => {
+  return (
+    <div>
+      <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
+        Admin Dashing
+      </h2>
+    </div>
+  );
+};
+
+export default AdminDashboardHome;
