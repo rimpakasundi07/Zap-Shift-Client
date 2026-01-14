7bb07e2a1555a997ceb294c551c16abddf90a237
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-14 22:43:00 +0530

    Update DashboardHome with conditional rendering based on user role

--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import useRole from "../../../hooks/useRole";
+import Loading from "../../../components/Loding/Loading";
+import AdminDashboardHome from "./AdminDashboardHome";
 
 const DashboardHome = () => {
+  const { role, roleLoading } = useRole();
+  if (roleLoading) {
+    return <Loading></Loading>;
+  }
+  if (role === "admin") {
+    return <AdminDashboardHome></AdminDashboardHome>;
+  }
+
   return (
     <div>
       <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
