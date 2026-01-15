77888960dd925ba1a9842e07df333da0a543a077
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-15 20:53:18 +0530

    Add AdminDashboardHome with stats cards and delivery status analytics

--- src/pages/Dashboard/DashboardHome/UserDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/UserDashboardHome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UserDashboardHome = () => {
   return (
     <div>
-      <h2 className="text-teal-800 py-2 lg:p-6 font-bold lg:text-4xl text-xl">
+      <h2 className="text-teal-800 py-2 text-center lg:py-10 font-bold lg:text-6xl text-xl">
         User Dashing
       </h2>
     </div>
